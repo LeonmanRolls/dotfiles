f3c34ca7186ef3823b989a2609da36dd1367e405
Leon Talbert <leon.talbert@gmail.com>
2014-01-17 18:18:53 +0000

    open vim and get dependencies automatically

--- linkup.sh
+++ linkup.sh
@@ -1,3 +1,7 @@
+git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+
 ln -s ~/dotfiles/.vim ~/.vim
 ln -s ~/dotfiles/.bashrc ~/.bashrc
 ln -s ~/dotfiles/.vimrc ~/.vimrc
+
+vim +BundleInstall +qall
